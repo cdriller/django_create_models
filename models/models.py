d696917b6cfbe58b5d5289b3e99febfca9a242a2
cdriller <carlosdriller16@gmail.com>
2020-07-01 14:05:07 +0200

    first try for K3 Models

--- models/models.py
+++ models/models.py
@@ -1,21 +1,75 @@
 from django.db import models
+from django.core.validators import RegexValidator
 
 class Company(models.Model):
+	name = models.CharField(max_length=80)
+	strasse = models.CharField(max_length=80)
+	plz = models.CharField(max_length=20)
+	ort = models.CharField(max_length=80)
+	land = models.CharField(max_length=2,validators=[RegexValidator(regex='^.{2}$', message='Length has to be 2', code='nomatch')])
+
+class Contact(models.Model):
+	company = models.ForeignKey(Company, on_delete=models.CASCADE)
+	anrede = models.CharField(max_length=80)
+	vorname = models.CharField(max_length=80)
+	zuname = models.CharField(max_length=80)
+	abteilung = models.CharField(max_length=80)
+	telefon = models.CharField(max_length=80)
+	fax = models.CharField(max_length=80)
+	email = models.EmailField()
+	extranet_id = models.CharField(max_length=8)
 
 class Report(models.Model):
-
-	absender = models.ForeignKey(Company, on_delete=models.SET_NULL)
-	ersteller = models.ForeignKey(Company, on_delete=models.SET_NULL)
-	melder = models.ForeignKey(Company, on_delete=models.SET_NULL)
+	absender = models.ForeignKey(
+		Company,  
+		null=True, 
+		on_delete=models.SET_NULL,
+		related_name="absender"
+	)
+	absender_kontakt = models.ForeignKey(
+		Contact,
+		null=True,
+		on_delete=models.SET_NULL,
+		related_name="absender_kontakt"
+	)
+	ersteller = models.ForeignKey(
+		Company, 
+		null=True, 
+		on_delete=models.SET_NULL,
+		related_name="ersteller"
+	)
+	ersteller_kontakt = models.ForeignKey(
+		Contact,
+		null=True,
+		on_delete=models.SET_NULL,
+		related_name="ersteller_kontakt"
+	)
+	melder = models.ForeignKey(
+		Company,
+		on_delete=models.CASCADE,
+		related_name="melder"
+	)
+	melder_kontakt = models.ForeignKey(
+		Contact,
+		null=True,
+		on_delete=models.SET_NULL,
+		related_name="melder_kontakt"
+	)
 
 	kommentar = models.TextField()
-	meldetermin =
-	bilanzstichtag =
-	formular
+	meldetermin = models.PositiveSmallIntegerField()
+	bilanzstichtag = models.DateField()
 
-	class Meta:
-		abstract = True
+class K3(models.Model):
+	report = models.ForeignKey(Report, null=True, on_delete=models.SET_NULL)
+	firma = models.ForeignKey(Company, on_delete=models.CASCADE)
 
-class K3(Report):
+class Balance_Sheet(models.Model):
+	k3 = models.OneToOneField(
+		K3,
+		on_delete=models.SET_NULL,
+		null=True
+	)
+	P01 = models.IntegerField()
 
-class K4(Report):
+# class K4(models.Model):
